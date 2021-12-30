7f0db5507259fcc247558074e43942b06940c305
TedLivist <livistteddy@gmail.com>
2021-12-30 09:19:33 +0100

    Build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'teddy', password: '12345')
+User.create(username: 'xvv', password: '12345')
+User.create(username: 'cvv', password: '12345')
