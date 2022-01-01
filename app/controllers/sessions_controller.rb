ef5c4ed6dd9279493db7de23a2dde5cb9ae9d9c2
TedLivist <livistteddy@gmail.com>
2022-01-01 02:29:41 +0100

    Add sessions#destroy and update navbar links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to login_path
+  end
 end
