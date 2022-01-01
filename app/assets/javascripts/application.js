be2ddf0fa887316e52cd79a2b3265c42d5cdebd3
TedLivist <livistteddy@gmail.com>
2022-01-02 00:38:08 +0100

    Display flash message and add js to close message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,4 +22,13 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+
+  $('.message .close')
+    .on('click', function() {
+      $(this)
+        .closest('.message')
+        .transition('fade')
+      ;
+    })
+  ;
 })
