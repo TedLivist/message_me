10d515f57a5e19f2b04a53672e5406096843a34e
TedLivist <livistteddy@gmail.com>
2022-01-01 02:02:06 +0100

    Structure sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
+  
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in!'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There was something wrong with your login information'
+      render 'new'
+    end
+  end
+
   def new
-    
   end
 end
